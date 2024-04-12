d5570ad3114d1c36e83b7516bb3091e66ac0068f
sgorgun <sergei.gorgun@gmail.com>
2024-04-12 14:39:59 +0300

    Guest User functionality in Data Access Layer is implemented according to the instructions in the task. But it violates DRY principle.

--- StoreDAL/Repository/ProductRepository.cs
+++ StoreDAL/Repository/ProductRepository.cs
@@ -5,44 +5,99 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StoreDAL.Data;
 
 namespace StoreDAL.Repository
 {
-    public class ProductRepository : IProductRepository
+    /// <summary>
+    /// Represents a repository for managing products.
+    /// </summary>
+    public class ProductRepository : AbstractRepository, IProductRepository
     {
+        private readonly DbSet<Product> dbSet;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProductRepository"/> class.
+        /// </summary>
+        /// <param name="context">The store database context.</param>
+        public ProductRepository(StoreDbContext context) : base(context)
+        {
+            dbSet = context.Set<Product>();
+        }
+
+        /// <summary>
+        /// Adds a new product to the repository.
+        /// </summary>
+        /// <param name="entity">The product entity to add.</param>
         public void Add(Product entity)
         {
-            throw new NotImplementedException();
+            dbSet.Add(entity);
+            context.SaveChanges();
         }
 
+        /// <summary>
+        /// Deletes a product from the repository.
+        /// </summary>
+        /// <param name="entity">The product entity to delete.</param>
         public void Delete(Product entity)
         {
-            throw new NotImplementedException();
+            dbSet.Remove(entity);
+            context.SaveChanges();
         }
 
+        /// <summary>
+        /// Deletes a product from the repository by its ID.
+        /// </summary>
+        /// <param name="id">The ID of the product to delete.</param>
+        /// <exception cref="InvalidOperationException">Thrown when the product with the specified ID is not found.</exception>
         public void DeleteById(int id)
         {
-            throw new NotImplementedException();
+            var entity = dbSet.Find(id);
+            if (entity == null) return;
+            dbSet.Remove(entity);
+            context.SaveChanges();
         }
 
-        public IEnumerable<Product> GetAll()
-        {
-            throw new NotImplementedException();
-        }
+        /// <summary>
+        /// Gets all products from the repository.
+        /// </summary>
+        /// <returns>An enumerable collection of all products.</returns>
+        public IEnumerable<Product> GetAll() => dbSet.ToList();
 
+        /// <summary>
+        /// Gets a paged collection of products from the repository.
+        /// </summary>
+        /// <param name="pageNumber">The page number.</param>
+        /// <param name="rowCount">The number of rows per page.</param>
+        /// <returns>An enumerable collection of products for the specified page.</returns>
+        /// <exception cref="ArgumentException">Thrown when the page number or row count is less than or equal to zero.</exception>
         public IEnumerable<Product> GetAll(int pageNumber, int rowCount)
         {
-            throw new NotImplementedException();
+            return pageNumber <= 0 || rowCount <= 0
+                ? throw new ArgumentException("Page number and row count must be greater than zero")
+                : dbSet.Skip((pageNumber - 1) * rowCount).Take(rowCount).ToList();
         }
 
+        /// <summary>
+        /// Gets a product by its ID from the repository.
+        /// </summary>
+        /// <param name="id">The ID of the product to get.</param>
+        /// <returns>The product entity with the specified ID.</returns>
+        /// <exception cref="InvalidOperationException">Thrown when the product with the specified ID is not found.</exception>
         public Product GetById(int id)
         {
-            throw new NotImplementedException();
+            return dbSet.Find(id) ?? throw new InvalidOperationException("User role with this ID was not found");
         }
 
+        /// <summary>
+        /// Updates a product in the repository.
+        /// </summary>
+        /// <param name="entity">The product entity to update.</param>
         public void Update(Product entity)
         {
-            throw new NotImplementedException();
+            dbSet.Update(entity);
+            context.SaveChanges();
         }
     }
 }
--- StoreDAL/Repository/UserRoleRepository.cs
+++ StoreDAL/Repository/UserRoleRepository.cs
@@ -10,52 +10,95 @@ using System.Threading.Tasks;
 
 namespace StoreDAL.Repository
 {
+    /// <summary>
+    /// Represents a repository for managing user roles.
+    /// </summary>
     public class UserRoleRepository : AbstractRepository, IUserRoleRepository
     {
         private readonly DbSet<UserRole> dbSet;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserRoleRepository"/> class.
+        /// </summary>
+        /// <param name="context">The store database context.</param>
         public UserRoleRepository(StoreDbContext context) : base(context)
         {
             dbSet = context.Set<UserRole>();
         }
+
+        /// <summary>
+        /// Adds a new user role to the repository.
+        /// </summary>
+        /// <param name="entity">The user role entity to add.</param>
         public void Add(UserRole entity)
         {
             dbSet.Add(entity);
             context.SaveChanges();
         }
 
+        /// <summary>
+        /// Deletes a user role from the repository.
+        /// </summary>
+        /// <param name="entity">The user role entity to delete.</param>
         public void Delete(UserRole entity)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Deletes a user role by its ID from the repository.
+        /// </summary>
+        /// <param name="id">The ID of the user role to delete.</param>
         public void DeleteById(int id)
         {
             var entity = dbSet.Find(id);
-            if (entity != null)
-            {
-                dbSet.Remove(entity);
-                context.SaveChanges();
-            }
+            if (entity == null) return;
+            dbSet.Remove(entity);
+            context.SaveChanges();
         }
 
+        /// <summary>
+        /// Gets all user roles from the repository.
+        /// </summary>
+        /// <returns>An enumerable collection of user roles.</returns>
         public IEnumerable<UserRole> GetAll()
         {
             return dbSet.ToList();
         }
 
-        public IEnumerable<UserRole> GetAll(int pageNumber, int RowCount)
+        /// <summary>
+        /// Gets a paged list of user roles from the repository.
+        /// </summary>
+        /// <param name="pageNumber">The page number.</param>
+        /// <param name="rowCount">The number of rows per page.</param>
+        /// <returns>An enumerable collection of user roles.</returns>
+        /// <exception cref="ArgumentException">Thrown when the page number or row count is less than or equal to zero.</exception>
+        public IEnumerable<UserRole> GetAll(int pageNumber, int rowCount)
         {
-            throw new NotImplementedException();
+            return pageNumber <= 0 || rowCount <= 0
+                ? throw new ArgumentException("Page number and row count must be greater than zero")
+                : dbSet.Skip((pageNumber - 1) * rowCount).Take(rowCount).ToList();
         }
 
+        /// <summary>
+        /// Gets a user role by its ID from the repository.
+        /// </summary>
+        /// <param name="id">The ID of the user role to get.</param>
+        /// <returns>The user role entity.</returns>
+        /// <exception cref="InvalidOperationException">Thrown when the user role with the specified ID is not found.</exception>
         public UserRole GetById(int id)
         {
-            return dbSet.Find(id);
+            return dbSet.Find(id) ?? throw new InvalidOperationException("User role with this ID was not found");
         }
 
+        /// <summary>
+        /// Updates a user role in the repository.
+        /// </summary>
+        /// <param name="entity">The user role entity to update.</param>
         public void Update(UserRole entity)
         {
-            throw new NotImplementedException();
+            dbSet.Update(entity);
+            context.SaveChanges();
         }
     }
 }
